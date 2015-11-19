cdc66be44e4bbded306f02508ffbb73dd5beca30
Steven Vore <steven@vorefamily.net>
2015-11-19 13:29:36 -0500

    extracted server to its own module

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var http = require("http");
+var url  = require("url");
 
-function onRequest(request, response) {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("<html><head><title>Hello.</title></head>");
-	response.write("<body>");
-	response.write("<p>Hello world!</p>");
-	response.write("</body></html>");	
-	response.end();
-};
+function start() {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("<html><head><title>Hello.</title></head>");
+		response.write("<body>");
+		response.write("<p>Hello world.</p>");
+		response.write("</body></html>");	
+		response.end();
+	}
 	
-http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(8888);
+	console.log("server has started.");
+}
+
+exports.start = start;
