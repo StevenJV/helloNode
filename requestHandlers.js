51e80cb735aa772d938b001c911dfdb6bcf80c2a
Steven Vore <steven@vorefamily.net>
2015-11-20 10:58:10 -0500

    start includes form, upload hanlder displays what was sent

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,11 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function header(response) {
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("<html><head><title>Hello.</title></head>");
 		response.write("<body>");	
-		response.write("<p><a href='ls'>directory listing</a> <a href='find'>all files</a></p>");
+		response.write("<p><a href='start'>start form</a></p> <a href='ls'>directory listing</a> <a href='find'>all files</a></p>");
 }
 
 function ls(response) {
@@ -31,20 +32,26 @@ function find(response) {
   	console.log("Request handler 'find' completed.");	
 }
 
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called.");
 	header(response);
-	response.write("Start");
-	response.write("</body></html>");	
+	var body = 	'<form action="/upload" method="post">'+
+				'<textarea name="text" rows="20" cols="60"></textarea>'+
+				'<input type="submit" value="Submit text" />'+
+				'</form>'+
+				'</body>'+
+				'</html>';
+	response.write(body);		
 	response.end();	
   	console.log("Request handler 'start' completed.");	
 	
 }
 
-function upload(response) {
+function upload(response, postData) {
+	var dataText = querystring.parse(postData).text;
 	console.log("Request handler 'upload' was called.");
 	header(response);
-	response.write("upload");
+	response.write("You sent " + dataText);
 	response.write("</body></html>");	
 	response.end();		
   	console.log("Request handler 'upload' completed.");	
--- server.js
+++ server.js
@@ -3,12 +3,23 @@ var url  = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		
-		//temporarily hide 'noise'
+		// hide favicon 'noise'
 		if (pathname != "/favicon.ico") {
 			console.log("Request for " + pathname + " received.");
-			route(handle, pathname, response);
+
+			request.setEncoding("utf8");
+			
+			request.addListener("data", function(postDataChunk) {
+				postData += postDataChunk;
+				console.log(" ** Received POST data chunk, size = '" + postDataChunk.length + "'.");
+			})
+
+			request.addListener("end", function() {			
+				route(handle, pathname, response, postData);
+			});
 		}
 	}
 	
