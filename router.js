47e198c8cad8eaf3e45f97eff072a78582951b91
Steven Vore <steven@vorefamily.net>
2016-08-03 11:35:41 -0400

    test commit

--- router.js
+++ router.js
@@ -5,10 +5,10 @@ function route(handle, pathname, response, request) {
 		} else {
 			console.log("No request handler found for " + pathname);
 			response.writeHead(404, {"Content-Type": "text/html"});
-			response.write("<html><head><title>404 Not found.</title></head>");
+			response.write("<html><head><title>404 Not found. </title></head>");
 			response.write("<body>");
 			response.write("<p>404 Not found.</p>");
-			response.write("</body></html>");	
+			response.write("</body></html>");
 			response.end();
 		}
 }
